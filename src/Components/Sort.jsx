44c579d236818262225c3781a7b9f076cb830444
Grigoriy Rusin <rusingrisha@gmail.com>
2023-09-18 01:00:46 +0500

    Логика попап соритровки

--- src/Components/Sort.jsx
+++ src/Components/Sort.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React from 'react';
 
 function Sort() {
+  const sortTypes = ['популярности', 'цене', 'алфавиту'];
+  const [activeSortType, setActiveSortType] = React.useState(sortTypes[0]);
+
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const onChooseSortType = (i) => {
+    setActiveSortType(sortTypes[i]);
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="sort">
       <div className="sort__label">
@@ -9,23 +18,29 @@ function Sort() {
           height="6"
           viewBox="0 0 10 6"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setIsOpen(!isOpen)}>{activeSortType}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {isOpen && (
+        <div className="sort__popup">
+          <ul>
+            {sortTypes.map((type, i) => (
+              <li
+                key={i}
+                onClick={() => onChooseSortType(i)}
+                className={activeSortType === sortTypes[i] ? 'active' : ''}>
+                {type}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
